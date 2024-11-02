06a470efcc7b9c2e7c8a054e55b9a396f1e944cf
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2024-11-02 13:41:14 +0100

    Dodano lab5 ćwiczeni 2

--- lab5.py
+++ lab5.py
@@ -1,19 +1,71 @@
+# from PIL import Image, ImageFilter
+# import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('TkAgg')  # Ustaw backend na TkAgg
+#
+# img = Image.open("D:/Repozytoria i inne takie/Wizja_Maszynowa/Wizja_Maszynowa/lab5.png")
+#
+# imggauss = img.filter(ImageFilter.GaussianBlur(3))
+#
+# plt.figure(1, figsize = (15, 10))
+# plt.subplot(131)
+# plt.title('Obraz przed rozmyciem')
+# plt.axis('off')
+# plt.imshow(img)
+# plt.subplot(132)
+# plt.title('Obraz po rozmyciu')
+# plt.axis('off')
+# plt.imshow(imggauss)
+# plt.show()
+
 from PIL import Image, ImageFilter
+import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
-matplotlib.use('TkAgg')  # Ustaw backend na TkAgg
 
-img = Image.open("D:/Repozytoria i inne takie/Wizja_Maszynowa/Wizja_Maszynowa/lab5.png")
+# Ustawienie backendu dla Matplotlib
+matplotlib.use('TkAgg')
 
+# Wczytanie i konwersja obrazu na skalę szarości
+img = Image.open("D:/Repozytoria i inne takie/Wizja_Maszynowa/Wizja_Maszynowa/lab5.png").convert('L')
+
+# Zastosowanie rozmycia Gaussa
 imggauss = img.filter(ImageFilter.GaussianBlur(3))
 
-plt.figure(1, figsize = (15, 10))
+# Wyświetlenie obrazu przed i po rozmyciu
+plt.figure(figsize=(15, 10))
 plt.subplot(131)
 plt.title('Obraz przed rozmyciem')
 plt.axis('off')
-plt.imshow(img)
+plt.imshow(img, cmap='gray')  # Dodano cmap='gray' dla poprawnej skali szarości
 plt.subplot(132)
 plt.title('Obraz po rozmyciu')
 plt.axis('off')
-plt.imshow(imggauss)
+plt.imshow(imggauss, cmap='gray')
 plt.show()
+
+# Konwersja obrazu do tablicy NumPy
+imgArray = np.asarray(img)
+
+# Progowanie obrazu przy różnych wartościach progowych
+_, imgThresholded1 = cv2.threshold(imgArray, 64, 255, cv2.THRESH_BINARY)
+_, imgThresholded2 = cv2.threshold(imgArray, 127, 255, cv2.THRESH_BINARY)
+_, imgThresholded3 = cv2.threshold(imgArray, 191, 255, cv2.THRESH_BINARY)
+
+# Wyświetlenie obrazów po progowaniu
+plt.figure(figsize=(15, 10))
+plt.subplot(131)
+plt.title('Próg 64')
+plt.axis('off')
+plt.imshow(imgThresholded1, cmap='gray')
+plt.subplot(132)
+plt.title('Próg 127')
+plt.axis('off')
+plt.imshow(imgThresholded2, cmap='gray')
+plt.subplot(133)
+plt.title('Próg 191')
+plt.axis('off')
+plt.imshow(imgThresholded3, cmap='gray')
+plt.show()
+
